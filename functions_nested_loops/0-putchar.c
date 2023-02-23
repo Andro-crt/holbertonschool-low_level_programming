cdd00ec70593e0a863ab5597220a807b17dd0020
Andro-crt <dmaswanganye@singular.co.za>
2023-02-23 00:43:30 -0800

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@ int main(void)
 {
    char *sh = "_putchar";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+   while (*sh)
+   {
+     _putchar(*sh);
+     sh++;
+   }
+   _putchar('\n');
 
-	return (0);
+   return (0);
 }
